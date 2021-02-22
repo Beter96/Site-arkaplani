3b49bf561b4ded4ce24bad2ad1263edd28b5a55f
Beter96 <79377159+Beter96@users.noreply.github.com>
2021-02-22 15:51:26 +0300

    Add files via upload

---
+++ <html>.sh
@@ -0,0 +1,8 @@
+<html>
+    <head>
+       <title>Sayfamızda görüntülenecek başlık</title>
+    </head>
+    <body background="https://www.pixelstalk.net/wp-content/uploads/2016/03/Free-Green-Backgrounds.jpg">
+       Mehmet Kirazlı Web Sitesi
+    </body>
+ </html>
